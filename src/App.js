81d372537855f52951616ed6eea0dfac3703f9b0
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-08-25 02:52:42 +0530

    Make the cannon move with mouse

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { getCanvasPosition } from './utils/formulas';
 import Canvas from './components/Canvas';
 
 class App extends Component {
+  componentDidMount() {
+    const self = this;
+    setInterval(() => {
+        self.props.moveObjects(self.canvasMousePosition);
+    }, 10);
+  }
+
+  trackMouse(event) {
+    this.canvasMousePosition = getCanvasPosition(event);
+  }
+
   render() {
     return (
-      <Canvas />
+      <Canvas
+        angle={this.props.angle}
+        trackMouse={event => (this.trackMouse(event))}
+      />
     );
   }
 }
 
-App.PropTypes={
-  message: PropTypes.string.isRequired
-}
+App.propTypes = {
+  angle: PropTypes.number.isRequired,
+  moveObjects: PropTypes.func.isRequired,
+};
+
 export default App;
